4438a099286914949498288674690bc6cbd0884f
Cristian Mora <cristianfmorac@gmail.com>
2023-07-10 21:51:29 -0500

    Estructura proyecto de tareas por hacer

--- node-0-experto/Node/proy-tareas-x-hacer/README.md
+++ node-0-experto/Node/proy-tareas-x-hacer/README.md
@@ -1,2 +1,6 @@
 # Insatalaciones
 Se debe instalar todos los paquetes de package.json con el comando `npm install`
+
+# paquetes
+colors = Colorear caracteres en consola
+inquirer = Desplazamiento en la consola con las flechas del teclado
--- node-0-experto/Node/proy-tareas-x-hacer/app.js
+++ node-0-experto/Node/proy-tareas-x-hacer/app.js
@@ -1,8 +1,17 @@
 require('colors')
+const {mostrarMenu, pausa} = require('./helpers/message')
+
 console.clear();
 
 const main = async() => {
     
+    let opt = ''
+    do {
+        // retorna un valor de la promesa
+        opt = await mostrarMenu();
+        console.log({opt});
+        if (opt !== '0') await pausa();
+    } while (opt !== '0');
 }
 
 main()
